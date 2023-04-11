ade8fb91d5bf12433466c78b9623f454d91fb84b
Borisfc23 <borisfc23@gmail.com>
2023-04-11 16:57:45 -0500

    THIRD commit

--- src/db.js
+++ src/db.js
@@ -1,10 +1,16 @@
 import { createPool } from "mysql2/promise"; //el pool es para un conjunto de conexiones
-import { DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD } from "./config.js";
+import {
+  DB_DATABASE,
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_PORT,
+} from "./config.js";
 const pool = createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
-  port: 3306,
+  port: DB_PORT,
 });
 export { pool };
